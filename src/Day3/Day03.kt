2168cf3716929568a02c37024ab9dac364b478f0
lukeqwas <undeadlizardcrypto@proton.me>
2022-12-07 19:48:15 -0500

    solution through day 4

--- src/Day3/Day03.kt
+++ src/Day3/Day03.kt
@@ -3,6 +3,45 @@ package Day3
 import readInput
 import java.lang.IllegalArgumentException
 
+// http://brianaspinall.com/wp-content/uploads/2015/11/better_ascii_table.jpg
+
 fun main() {
-    val testInput = readInput("Day3/Day03_test")
+    val testInput = readInput("Day3/Day03")
+    var prioritySum = 0
+    var index = 1;
+    val curList = mutableListOf<String>()
+    for(input in testInput) {
+        // group them into 3
+        curList.add(input)
+        if(index % 3 == 0) {
+            val sharedItem = findSharedItem(curList)
+            prioritySum += getPriority(sharedItem)
+            curList.clear()
+        }
+        index++
+    }
+    println(prioritySum)
+}
+
+fun findSharedItem(items: List<String>): Char {
+    val itemSet1 = mutableSetOf<Char>()
+    itemSet1.addAll(items[0].map { it })
+    val itemSet2 = mutableSetOf<Char>()
+    itemSet2.addAll(items[1].map { it })
+    val itemSet3 = mutableSetOf<Char>()
+    itemSet3.addAll(items[2].map { it })
+    for(item1 in itemSet1) {
+        if(itemSet2.contains(item1) && itemSet3.contains(item1)) {
+            return item1
+        }
+    }
+    throw IllegalArgumentException("No shared elements between first:  and second ")
+}
+
+fun getPriority(item: Char): Int {
+    if(item.isUpperCase()) {
+        return (item.toInt()) - 38
+    } else {
+        return (item.toInt()) - 96
+    }
 }
---
+++ src/Day4/Day04.kt
@@ -0,0 +1,39 @@
+package Day4
+
+import readInput
+
+//data class Range(val lowerBound: Int, val upperBound: Int)
+
+
+
+fun main() {
+    val testInput = readInput("Day4/Day04")
+    var numPairs = 0
+
+    for(input in testInput) {
+        // find the smallest lower bound and see if the other range falls in the smalles lower bound range
+        val ranges = input.split(',')
+        val range1 = Range(ranges[0].split('-')[0].toInt(), ranges[0].split('-')[1].toInt())
+        val range2 = Range(ranges[1].split('-')[0].toInt(), ranges[1].split('-')[1].toInt())
+        if(range1.lowerBound >= range2.lowerBound && range1.lowerBound <= range2.upperBound) {
+            numPairs++
+        } else if(range2.lowerBound >= range1.lowerBound && range2.lowerBound <= range1.upperBound) {
+            numPairs++
+        }
+    }
+    println(numPairs)
+
+
+}
+
+class Range(private val num1: Int, private val num2: Int) {
+    val lowerBound: Int
+    val upperBound: Int
+    init {
+        lowerBound = Math.min(num1, num2)
+        upperBound = Math.max(num1, num2)
+    }
+    fun isSmaller(otherRange: Range): Boolean {
+        return Math.min(this.lowerBound, this.upperBound) < Math.min(otherRange.lowerBound, otherRange.upperBound)
+    }
+}
